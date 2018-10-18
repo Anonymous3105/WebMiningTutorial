90f2661ccd661e7445611de9ae136aded9b89f1e
Anonymous3105 <abhijeetambadekar057@gmail.com>
2018-10-18 14:59:07 +0530

    Added NBC L10

---
+++ L10_NaiveBayes/WM_L13_1156.py
@@ -0,0 +1,117 @@
+import pandas as pd
+# import math
+from operator import itemgetter
+from pprint import pprint
+
+def preprocess_data(data, return_thresholds=False):
+	columns = data.columns
+	thresholds = []
+	for column in columns[:-1]:
+		min_val = 0
+		max_val = data.loc[:, column].max()
+		threshold = (max_val + min_val)/2
+		thresholds.append(threshold)
+		data.loc[(data[column] <= threshold), column] = 0
+		data.loc[(data[column] > threshold), column] = 1
+	thresholds = pd.DataFrame({'column': columns[:-1], 'threshold': thresholds}).set_index(keys = ['column'])
+	# print(thresholds.head(len(columns[:-1])))
+	# print('\n')
+	if return_thresholds:
+		return data, thresholds
+	else:
+		return data
+
+
+class NBClassifier():
+	def __init__(self):
+		pass
+
+	def load_data(self, train_data, target=None):
+		self.train_data = train_data
+		if target:
+			self.target = self.train_data.columns[-1]
+		else:
+			self.target = target
+
+		self.outcomes = self.train_data[self.target].unique()
+
+
+		self.outcomeProb = {}
+		total_count = self.train_data.shape[0]
+		for outcome in self.outcomes:
+			count = self.train_data.loc[(self.train_data[self.target] == outcome)].shape[0]
+			self.outcomeProb[outcome] = count/total_count
+		
+
+		self.probTable = pd.DataFrame(columns = ['key'].append(self.outcomes))
+		for column in train_data.columns[:-1]:
+			for entry in train_data[column].unique():
+				temp = {}
+				temp['key'] = column + '_' + str(entry)
+				for outcome in self.outcomes:
+					temp[outcome] = self.calcProb(column, entry, outcome)
+				self.probTable = self.probTable.append(pd.DataFrame(temp.copy(), index = [0]), ignore_index = True)
+		self.probTable = self.probTable.set_index('key')
+
+		print("\nThe probabilites for each entry for each column is calculated as follows: ")
+		pprint(self.probTable)
+
+	def calcProb(self, column, entry, outcome):
+		tot_count = self.train_data.loc[self.train_data[self.target] == outcome].shape[0]
+		count = self.train_data.loc[(self.train_data[column] == entry) & (self.train_data[self.target] == outcome)].shape[0]
+		return count/tot_count
+
+
+	def predict(self, test_data):
+		labels = []
+		for _, test_instance in test_data.iterrows():
+			results = {}
+			for outcome in self.outcomes:
+				results[outcome] = 1
+				for key in test_instance.keys():
+					results[outcome] *= self.probTable.loc[key + '_' + str(test_instance.loc[key]), outcome]
+				results[outcome] *= self.outcomeProb[outcome]
+			labels.append(max(results.items(), key=itemgetter(1))[0])
+		return list(enumerate(labels))
+					
+
+
+print("Lab Question Part - A --------------------------------------------------\n")
+
+train_data = pd.read_csv("train_A.csv")
+print("\nThe training data looks like :")
+pprint(train_data)
+
+target = "Buys_computer"
+
+test_data = pd.read_csv("test_A.csv")
+print("\nThe testing data looks like :")
+pprint(test_data)
+
+nbc = NBClassifier()
+nbc.load_data(train_data, target)
+print("\nThe output for the test instances are: \n")
+for outcome in nbc.predict(test_data):
+	print(outcome[0], ": ", outcome[1])
+
+
+print("\nLab Question Part - B --------------------------------------------------\n")
+
+train_data = pd.read_csv("train_B.csv")
+train_data, threasholds = preprocess_data(train_data, return_thresholds=True)
+print("\nThe training data after preprocessing looks like :")
+pprint(train_data)
+
+target = "Category"
+
+test_data = pd.read_csv("test_B.csv")
+test_data = preprocess_data(test_data)
+print("\nThe testing data after preprocessing looks like :")
+pprint(test_data)
+
+
+nbc2 = NBClassifier()
+nbc2.load_data(train_data, target)
+print("\nThe output for the test instances are: \n")
+for outcome in nbc2.predict(test_data):
+	print(outcome[0], ": ", outcome[1])
---
+++ L9_CosineSimilarity/L9_CosineSimCluster.py
@@ -0,0 +1,107 @@
+import numpy as np
+from pprint import pprint
+from math import inf, sqrt
+
+
+def get_word_count(words, text):
+	if type(words) == str:
+		return text.count(words)
+	elif type(words) == list:
+		return sum([text.count(wordi) for wordi in words])
+
+
+class CosineSimilarityClusterer():
+	def __init__(self, threshold=0.25):
+		self.threshold = threshold
+		self.wordlist = []
+		self.doclist = {}
+		self.doc_vecs = {}
+		
+		self.clusters = {}
+
+	def get_text_data(self, docfile):
+		with open(docfile) as fp:
+			content = list(map(str.lower, fp.readlines()))
+
+		for textline in content:
+			docname, doctext = textline.split(" : ")
+			self.doclist[docname] = doctext
+
+	def get_word_freq(self):
+		for doc in self.doclist:
+			self.doc_vecs[doc] = np.array([get_word_count(word, self.doclist[doc]) for word in self.wordlist])
+
+	def get_dot_prod(self, doc1, doc2):
+		return self.doc_vecs[doc1].T.dot(self.doc_vecs[doc2])
+
+	def get_doc_length(self, doc):
+		return sqrt(self.get_dot_prod(doc,doc))
+
+	def get_cos_angle(self, doc1, doc2):
+		return self.get_dot_prod(doc1, doc2) / (self.get_doc_length(doc1) * self.get_doc_length(doc2))
+
+
+	def get_nearest_doc(self, i, doc):
+		return sorted(self.doc_vecs.keys())[max(range(len(self.doc_vecs)), key=lambda k: self.cos_mat[i][k])]
+
+	def compute_cosine_matrix(self):
+		for i, doci in enumerate(sorted(self.doc_vecs.keys())):
+			for j, docj in enumerate(sorted(self.doc_vecs.keys())):
+				if i == j:
+					self.cos_mat[i][j] = -inf # Similar docs will have cos value as 1 and will be redundant in the calculation 
+				else:
+					self.cos_mat[i][j] = round(self.get_cos_angle(doci, docj), 2)
+
+
+	def fit(self, wordlist, docfile):
+		self.wordlist = wordlist
+		self.get_text_data(docfile)
+		self.get_word_freq()
+
+		self.cos_mat = [[0 for i in range(len(self.doc_vecs))] for j in range(len(self.doc_vecs))]
+		self.compute_cosine_matrix()
+
+	def cluster(self):
+		clusters = []
+		for i, doc in enumerate(sorted(self.doc_vecs.keys())):
+			nearest_doc = self.get_nearest_doc(i, doc)
+			clusters.append({doc, nearest_doc})
+
+		end_clusters = []
+		
+		while clusters:
+			flag_merged = True
+			first = clusters.pop(0)
+			while flag_merged:
+				flag_merged = False
+				for i in range(len(clusters)):
+					if first.intersection(clusters[i]):
+						first.update(clusters[i])
+						clusters[i] = set()
+						flag_merged = True
+
+			clusters = [j for j in clusters if len(j) != 0]
+			end_clusters.append(first)
+
+		self.clusters = {"Cluster "+str(i):end_clusters[i] for i in range(len(end_clusters))}
+
+
+	def print_details(self):
+		print("The document vectors look as follows:")
+		pprint(self.doc_vecs)
+
+		print("\nThe cosine similarity matrix looks as follows: ")
+		pprint(self.cos_mat)
+
+		print("\nThe documents are clustered using the Nearest Neighbour method as follows:")
+		pprint(self.clusters)
+
+
+
+wordlist = ["automotive", "car", "motorcycle", "self-drive", "iot", "hire", "dhoni"]
+docfile = "docfile.txt"
+
+csc = CosineSimilarityClusterer()
+csc.fit(wordlist, docfile)
+csc.cluster()
+csc.print_details()
